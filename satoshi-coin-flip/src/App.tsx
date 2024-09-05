77c2a76dac75971dae895ea3b0685c4254e4c1c2
zipper393 <146298386+zipper393@users.noreply.github.com>
2024-09-05 23:01:35 +0700

    Update App.tsx

--- satoshi-coin-flip/src/App.tsx
+++ satoshi-coin-flip/src/App.tsx
@@ -39,7 +39,7 @@ function App() {
         </Callout.Root>
         <Box style={{ background: 'var(--gray-a2)', borderRadius: 'var(--radius-3)' }}>
           <Container>
-           <Flex gap="3" align="center"
+           <Flex gap="3" align="center">
           <img src="https://i.ibb.co.com/4M4kKMV/head.png" alt="head" style={{
       objectFit: 'cover',
       width: '50%',
