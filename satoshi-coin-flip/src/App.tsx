258740919182a4f237d5910323eb6cddfafb4909
zipper393 <146298386+zipper393@users.noreply.github.com>
2024-09-19 18:09:02 +0700

    Update App.tsx

--- satoshi-coin-flip/src/App.tsx
+++ satoshi-coin-flip/src/App.tsx
@@ -1,10 +1,9 @@
-
 import { ConnectButton, useCurrentAccount } from "@mysten/dapp-kit";
-import { Box, Callout, Container, Flex, Grid, Heading, Link } from "@radix-ui/themes";
+import { Avatar, Box, Container, Flex, Grid, Heading, Link } from "@radix-ui/themes";
 import { PlayerSesh } from "./containers/Player/PlayerSesh";
 import { HouseSesh } from "./containers/House/HouseSesh";
-import { InfoCircledIcon } from "@radix-ui/react-icons";
-
+import ReactPlayer from 'react-player/youtube';
+import AccordionDemo from './accord.tsx';
 
 
 function App() {
@@ -21,54 +20,61 @@ function App() {
           background: 'var(--gray-a2)',
         }}
       >
-        <Box>
-          <Heading>LUCKY FLIP</Heading>
-        </Box>
+        <Flex gap="2">
+          <Avatar
+            src="https://i.ibb.co.com/8gD1x1f/output-onlinegiftools.gif"
+            fallback="Luck Flip"
+          />
+          <Avatar fallback="Luck Flip" />
+        </Flex>
+        
 
         <Box>
           <ConnectButton />
         </Box>
       </Flex>
       <Container>      
-       <Callout.Root mb="2" style={{padding: '20px', margin: '5px',}}>
-          <Callout.Icon>
-            <InfoCircledIcon />
-          </Callout.Icon>
-          <Callout.Text>
-            You need to connect to wallet that publish the smart contract
-            package
-          </Callout.Text>
-        </Callout.Root>
-        <Box style={{ background: 'var(--gray-a2)', borderRadius: 'var(--radius-3)' }}>
+      
+   
+        {!account ? (
+                   <>
+        <Heading mb="2" mt="2" size="7" color="amber">
+          HOW TO PLAY
+        </Heading>
+        <Container size="1" style={{ background: 'var(--gray-a2)', borderRadius: 'var(--radius-3)'}}>
+        <Flex gap="2" align="center">
+         <ReactPlayer 
+            url='https://youtu.be/QjxPr8CU6OY?si=pmIdGxLqiRJIEpwb'
+              muted={true}
+              playing={true}
+              width={'65%'}
+              controls={true} />
+        <AccordionDemo />
+        </Flex>
+        </Container>
+              </>
+        ) : (
+          <Container size="1" style={{ background: 'var(--gray-a2)', borderRadius: 'var(--radius-3)' }}>
+          <Grid columns="1" gap={"3"} width={"auto"}>
+          <Box style={{ background: 'var(--gray-a2)', borderRadius: 'var(--radius-3)' }}>
           <Container>
-           <Flex gap="3" align="center">
+           <Flex mt="6" gap="3" align="center">
           <img src="https://i.ibb.co.com/8gD1x1f/output-onlinegiftools.gif" alt="head" style={{
-      objectFit: 'cover',
-      width: '50%',
-      height: '100%',
+      objectFit: 'fill',
+      width: '35%',
+      height: '40%',
       borderRadius: 'var(--radius-2)',
     }}/>
            </Flex>
           </Container>
           <Heading size="9" align="center" style={{margin: '2px', paddingBottom: '40px', color: '#E0B20B',}}>
-            LUCKY FLIP
+            LUCK FLIP
           </Heading>
           
         </Box>
-        {!account ? (
-          <Heading size="4" align="center">
-            Please connect wallet to continue
-          </Heading>
-        ) : (
-          <Container size="1" style={{ background: 'var(--gray-a2)', borderRadius: 'var(--radius-3)' }}>
-          <Grid columns="2" gap={"3"} width={"auto"}>
-            <PlayerSesh />
             <HouseSesh />
-          </Grid>
-          </Container>
-        )}
-      </Container>
-      <footer style={{padding: "20px"}}>
+            <PlayerSesh />
+            <footer style={{padding: "20px"}}>
         <Container>
         <Flex
         position="sticky"
@@ -81,7 +87,7 @@ function App() {
         }}
       >
         <Box>
-          <Heading>LUCKY FLIP</Heading>
+          <Heading>LUCK FLIP</Heading>
         </Box>
         <Box>
           <div>
@@ -89,17 +95,17 @@ function App() {
           </div>
           <div>
           <label>
-            <Link href="https://discord.gg/SHT8yy5Zbt"> Discord</Link>
+            <Link underline="hover" href="https://discord.gg/SHT8yy5Zbt"> Discord</Link>
           </label>
           </div>
           <div>
           <label>
-          <Link href="https://t.me/luckflips"> Telegram</Link>
+          <Link underline="hover" href="https://t.me/luckflips"> Telegram</Link>
           </label>
           </div>
           <div>
           <label>
-          <Link href="https://x.com/flip_luck7"> Twitter</Link>
+          <Link underline="hover" href="https://x.com/flip_luck7"> Twitter</Link>
           </label>
           </div>
         </Box>
@@ -107,6 +113,11 @@ function App() {
       
         </Container>
       </footer>
+          </Grid>
+          </Container>
+        )}
+      </Container>
+      
     </>
   );
 }
