9acc369ced615e8c642158f2ef63e627ecd8d48f
zipper393 <146298386+zipper393@users.noreply.github.com>
2024-09-19 18:10:21 +0700

    Update accord.tsx

--- satoshi-coin-flip/src/accord.tsx
+++ satoshi-coin-flip/src/accord.tsx
@@ -1,1 +1,31 @@
+import * as Accordion from '@radix-ui/react-accordion';
+import './style.css';
 
+function AccordionDemo(){
+    return (
+        <Accordion.Root className="AccordionRoot" type="single" defaultValue="item-1" collapsible>
+    <Accordion.Item className="AccordionItem" value="item-1">
+      <Accordion.AccordionTrigger>Why Luck Flip?</Accordion.AccordionTrigger>
+      <Accordion.AccordionContent>Because it will bring u luck.</Accordion.AccordionContent>
+    </Accordion.Item>
+
+    <Accordion.Item className="AccordionItem" value="item-2">
+      <Accordion.AccordionTrigger>Is it Safe?</Accordion.AccordionTrigger>
+      <Accordion.AccordionContent>
+        Yes.
+      </Accordion.AccordionContent>
+    </Accordion.Item>
+
+    <Accordion.Item className="AccordionItem" value="item-3">
+      <Accordion.AccordionTrigger>How To Play?</Accordion.AccordionTrigger>
+      <Accordion.Content className="AccordionContent">
+        <div className="AccordionContentText">
+          You can see video right here!!
+        </div>
+      </Accordion.Content>
+    </Accordion.Item>
+  </Accordion.Root>
+ );
+
+}
+export default AccordionDemo;
